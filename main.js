83125cec06a89c74425b0dba6c59ae5ca99856a3
dingdong <dong.peng@hipac.cn>
2020-08-03 23:57:45 +0800

    ToyReactLearning

---
+++ main.js
@@ -0,0 +1,22 @@
+import {ToyReact,Component} from "./ToyReact.js";
+class MyComponent extends Component{
+    render(){
+        return <div>
+            <span>hello</span>
+            <span>world</span>
+        </div>
+    }
+    setAttribute(name,value){
+        this[name] = value;
+    }
+    mountTo(parent){
+        let vdom = this.render();
+        vdom.mountTo(parent);
+    }
+}
+let a = <MyComponent name = "a" id="ida"></MyComponent>
+       
+ToyReact.render(
+    a,
+    document.body
+)
